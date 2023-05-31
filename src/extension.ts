f97a2adec3fdd948c1e8f79e1cc1c5cbde9e99aa
jinpill <jinlee@metamorp.cc>
2023-05-31 22:21:26 +0900

    🐛 Fix a bug that prevent to replace selected text

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from "vscode";
 import calculate from "./features/calculate";
 import px2rem from "./features/px2rem";
+import rem2px from "./features/rem2px";
 
 // 확장프로그램이 실행될 때 가장 먼저 실행되는 메서드.
 export const activate = (context: vscode.ExtensionContext) => {
@@ -16,20 +17,32 @@ export const activate = (context: vscode.ExtensionContext) => {
     if (!input) return;
 
     editor.edit((editBuilder) => {
-      const position = editor.selection.active;
-      if (!position) return;
+      // 입력한 텍스트를 선택된 영역에 대체.
+      const applyText = (result: string) => {
+        const start = editor.selection.start;
+        const end = editor.selection.end;
+        const range = new vscode.Range(start.line, start.character, end.line, end.character);
+        editBuilder.replace(range, result);
+      };
 
       // 연산식인 경우 실행.
       if (calculate.check(input)) {
         const result = calculate.run(input);
-        editBuilder.insert(position, result);
+        applyText(result);
         return;
       }
 
       // to rem 키워드가 포함된 경우 실행.
       if (px2rem.check(input)) {
         const result = px2rem.run(input);
-        editBuilder.insert(position, result);
+        applyText(result);
+        return;
+      }
+
+      // to px 키워드가 포함된 경우 실행.
+      if (rem2px.check(input)) {
+        const result = rem2px.run(input);
+        applyText(result);
         return;
       }
     });
